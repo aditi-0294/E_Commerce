c424807372a130639cc3a9fe81fa2792d224401b
Aditi <a.aditi0294@rediffmail.com>
2017-08-09 21:56:10 +0530

    commit from previous files

---
+++ BookstoreBackend/src/main/java/com/niit/BookstoreBackend/dao/SupplierDAO.java
@@ -0,0 +1,60 @@
+package com.niit.BookstoreBackend.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.niit.BookstoreBackend.model.Supplier;
+
+@Repository(value = "supplierDAO") // marks specific class as DAO thus clarifying its' role
+public class SupplierDAO {
+
+	
+	@Autowired // tells container to inject a dependency
+	SessionFactory sessionFactory;
+	
+	public SupplierDAO(SessionFactory sessionFactory)
+	{
+		this.sessionFactory=sessionFactory;
+	}
+
+	
+	@Transactional
+	public void insertUpdateSupplier(Supplier supplier)
+	{
+		Session session=sessionFactory.getCurrentSession();
+		session.saveOrUpdate(supplier);
+	}
+	
+	
+	public Supplier getSupplier(int supid) {
+		
+		Session session=sessionFactory.openSession();
+		Supplier supplier =(Supplier)session.get(Supplier.class,supid);
+		session.close();
+		return supplier;
+	}
+	
+	
+	@Transactional
+	public void deleteSupplier(Supplier supplier) {
+		sessionFactory.getCurrentSession().delete(supplier);
+	}
+	
+	
+	public List<Supplier> getSupplierDetails()
+	{
+		Session session=sessionFactory.openSession();
+		Query query=session.createQuery("from Supplier");
+		List<Supplier> list=query.list();
+		session.close();
+		return list;
+	}
+	
+}
